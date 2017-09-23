e65c968db66fdb18bd6e3a2ff1b7f6bc48c6ceb4
zl2290103097 <2290103097@qq.com>
2017-09-23 19:44:10 +0800

    测试提交

--- UEHTML/UEHTML/Publish/Controller/ATQPublishViewController.h
+++ UEHTML/UEHTML/Publish/Controller/ATQPublishViewController.h
@@ -7,6 +7,7 @@
 //
 
 #import "ATQBaseViewController.h"
+#import "ComplainViewController.h"
 
 @interface ATQPublishViewController : ATQBaseViewController
 
---
+++ UEHTML/UEHTML/Publish/Controller/ComplainViewController.h
@@ -0,0 +1,13 @@
+//
+//  ComplainViewController.h
+//  UEHTML
+//
+//  Created by apple on 2017/9/21.
+//  Copyright © 2017年 LHKH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ComplainViewController : UIViewController
+
+@end

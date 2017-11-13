118ec0fbc9ae67459aa7016c6019a92d02f56c87
zl2290103097 <2290103097@qq.com>
2017-11-13 23:01:43 +0800

    需求详情页接口完成

--- UEHTML/UEHTML/Publish/DetailSubPublish/ViewController/DetailXuQiuViewController.m
+++ UEHTML/UEHTML/Publish/DetailSubPublish/ViewController/DetailXuQiuViewController.m
@@ -52,15 +52,36 @@
         }
             break;
         case 3:{
-            return 100.0f;
+            CGFloat textHeight = [self textHeight:self.currentModel.job.introduce];
+            NSLog(@"文字高度为：%.1f",textHeight);
+            if (textHeight <20.0) {
+                return 30.0+20.0+10.0;
+            }
+            else{
+                return 30.0+textHeight+10.0;
+            }
         }
             break;
         case 4:{
-            return 100.0f;
+            CGFloat textHeight = [self textHeight:self.currentModel.job.service_address];
+            NSLog(@"文字高度为：%.1f",textHeight);
+            if (textHeight <20.0) {
+                return 30.0+20.0+10.0;
+            }
+            else{
+                return 30.0+textHeight+10.0;
+            }
         }
             break;
         case 5:{
-            return 100.0f;
+            CGFloat textHeight = [self textHeight:self.currentModel.job.age];
+            NSLog(@"文字高度为：%.1f",textHeight);
+            if (textHeight <20.0) {
+                return 30.0+20.0+10.0;
+            }
+            else{
+                return 30.0+textHeight+10.0;
+            }
         }
             break;
         default:
@@ -80,6 +101,8 @@
             cell.nicknameLabel.text = self.currentModel.user_profile.nick_name;
             cell.chengLabel.text = self.currentModel.user_profile.credit_num;
             cell.juliLabel.text = self.currentModel.distance;
+            [cell.carImage sd_setImageWithURL:[NSURL URLWithString:self.currentModel.user_profile.car_logo] placeholderImage:[UIImage imageNamed:@"fabu-car"]];
+            
             
             return cell;
         }
@@ -90,6 +113,10 @@
             if (cell == nil) {
                 cell = [[NSBundle mainBundle] loadNibNamed:@"DetailXuQiuViewCellTableViewCell" owner:self options:nil][1];
             }
+            cell.jobNameLabel.text = self.currentModel.job.job_class_name;
+            cell.priceLabel.text = [NSString stringWithFormat:@"%@元/小时",self.currentModel.job.price];
+            cell.shiChangLabel.text = [NSString stringWithFormat:@"%@小时",self.currentModel.job.service_hourse];
+            
             
             return cell;
         }
@@ -101,6 +128,9 @@
                 cell = [[NSBundle mainBundle] loadNibNamed:@"DetailXuQiuViewCellTableViewCell" owner:self options:nil][2];
             }
             
+            cell.timeLabel.text = self.currentModel.job.start_time;
+            
+            
             return cell;
         }
             break;
@@ -110,7 +140,9 @@
             if (cell == nil) {
                 cell = [[NSBundle mainBundle] loadNibNamed:@"DetailSubPublishCell" owner:self options:nil][2];
             }
-            
+            [cell.leftImage setImage:[UIImage imageNamed:@"fabu-jieshao"]];
+            cell.titleNameLabel.text = @"需求说明";
+            cell.contentLabel.text = self.currentModel.job.introduce;
             return cell;
         }
             break;
@@ -120,6 +152,9 @@
             if (cell == nil) {
                 cell = [[NSBundle mainBundle] loadNibNamed:@"DetailSubPublishCell" owner:self options:nil][2];
             }
+            [cell.leftImage setImage:[UIImage imageNamed:@"fabu-location"]];
+            cell.titleNameLabel.text = @"TA的地址";
+            cell.contentLabel.text = self.currentModel.job.service_address;
             
             return cell;
         }
@@ -130,7 +165,9 @@
             if (cell == nil) {
                 cell = [[NSBundle mainBundle] loadNibNamed:@"DetailSubPublishCell" owner:self options:nil][2];
             }
-            
+            [cell.leftImage setImage:[UIImage imageNamed:@"fabu-fuwu"]];
+            cell.titleNameLabel.text = @"其他要求";
+            cell.contentLabel.text = [NSString stringWithFormat:@"%@  %@",self.currentModel.job.gender,self.currentModel.job.age];
             return cell;
         }
             break;
@@ -152,6 +189,12 @@
     // Dispose of any resources that can be recreated.
 }
 
+-(CGFloat)textHeight:(NSString *)string{
+    //传字符串返回高度
+    CGRect rect =[string boundingRectWithSize:CGSizeMake(SIZE_WIDTH-50.0, 9999) options:NSStringDrawingUsesLineFragmentOrigin attributes:@{NSFontAttributeName:[UIFont systemFontOfSize:14]} context:nil];//计算字符串所占的矩形区域的大小
+    return rect.size.height;//返回高度
+    
+}
 
 #pragma mark ===================懒加载==================
 - (NSMutableArray *)modelArray{
